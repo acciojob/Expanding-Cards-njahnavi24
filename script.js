bda8f90ca2237ff191cf6943194f9aad09bf07d8
njahnavi24 <jahnavinagavalli24@gmail.com>
2026-08-22 20:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+
+    panels.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    panel.classList.add("active");
+  });
+});
